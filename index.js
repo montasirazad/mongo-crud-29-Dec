7a05b76d9205b82184be2ae9dbea433e453da96f
unknown <montasirazad@gmail.com>
2021-12-29 05:58:08 +0600

    loaded data of single item

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+const ObjectId = require('mongodb').ObjectId;
 
 // MongoDb setup
 const { MongoClient } = require('mongodb');
+const req = require("express/lib/request");
 const uri = "mongodb+srv://azad:azad1403@cluster0.wov4a.mongodb.net/store?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -17,10 +19,10 @@ client.connect(err => {
     // Add item to db
     app.post('/addProduct', (req, res) => {
         const product = req.body;
-       // console.log(product);
+        // console.log(product);
         collection.insertOne(product)
             .then(result => {
-               // console.log(result,product);
+                // console.log(result,product);
                 res.send('success')
             })
 
@@ -28,12 +30,31 @@ client.connect(err => {
 
 
     // GET data 
-    app.get('/products', (req,res) => {
+    app.get('/products', (req, res) => {
         collection.find({})
-        .toArray( (err,documents) =>{
-            res.send(documents);
-           
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+
+            })
+    })
+
+    // Delete item 
+    app.delete('/deleteItem/:id', (req, res) => {
+        collection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                console.log(result);
+            })
+        //console.log(req.params.id);
+    })
+
+    // Load single product info
+
+    app.get('/item/:id', (req, res) => {
+        collection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+
     })
 });
 
