4c38e4edf0513e7611513ca71c6aca6fc96db09c
unknown <montasirazad@gmail.com>
2021-12-29 04:38:56 +0600

    displayed data on UI

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const express = require("express")
+// Dependencies
 
+const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
+// MongoDb setup
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://azad:azad1403@cluster0.wov4a.mongodb.net/store?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("store").collection("items");
-  console.log('db connected');
-  const product = {name:'Oil',price:100,quantity:20}
-  collection.insertOne(product)
-  .then(result => {
-      console.log('added');
-  })
+    const collection = client.db("store").collection("items");
+    console.log('db connected');
+
+    // Add item to db
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+       // console.log(product);
+        collection.insertOne(product)
+            .then(result => {
+               // console.log(result,product);
+                res.send('success')
+            })
+
+    })
+
+
+    // GET data 
+    app.get('/products', (req,res) => {
+        collection.find({})
+        .toArray( (err,documents) =>{
+            res.send(documents);
+           
+        })
+    })
 });
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log('listening at port 4000');
 })
